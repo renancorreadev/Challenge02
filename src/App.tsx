3f2123519a829a37324acb321e39280d423255a4
SkyXcripto {dev} <producer@skynance.net>
2021-11-03 15:27:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.scss'
+import { SideBar } from './components/SideBar'
+import { Content } from './components/Content'
+
+export function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar />
+      <Content />
+    </div>
+  )
+}
